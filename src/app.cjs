de626c3974c62fc97818df831c96084c30094b1e
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-22 01:59:51 +0530

    chore: added globals

--- src/app.cjs
+++ src/app.cjs
@@ -10,13 +10,16 @@ const validateEnv = require("./utils/validate.env.cjs");
 const cors = require("cors");
 const notFoundHandler = require("../src/middlewares/notFound.middleware.cjs");
 const errorHandler = require("../src/middlewares/errorHandle.middleware.cjs");
+const cookieParser = require("cookie-parser");
 // import routes
+const authRouter = require("./resources/auth/auth.routes.cjs");
 const userRouter = require("./resources/user_management/user.routes.cjs");
 
 validateEnv();
 // middlewares
 app.use(cors());
 app.use(express.json());
+app.use(cookieParser(process.env.COOKIE_SECRET));
 
 app.get("/", (req, res) => {
   res.send("Welcome to Pern Application");
@@ -24,6 +27,7 @@ app.get("/", (req, res) => {
 
 // initialize routes
 app.use("/api/v1/users", userRouter);
+app.use("/api/v1/auth", authRouter);
 
 app.use(notFoundHandler);
 app.use(errorHandler);
---
+++ src/utils/global.cjs
@@ -0,0 +1,11 @@
+const _15MINS = 1000 * 60 * 15;
+const _1HR = 1000 * 60 * 60;
+const _1DAY = 24 * _1HR;
+const _3DAY = 3 * _1DAY;
+
+module.exports = {
+  _15MINS,
+  _1HR,
+  _1DAY,
+  _3DAY,
+};

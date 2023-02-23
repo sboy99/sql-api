59345a0096afd759e722f56ca4a1246537d11874
Sagar Bera <contact.sagarbera@gmail.com>
2023-02-23 17:20:28 +0530

    feat: added stringify utility

--- src/utils/getConditionString.cjs
+++ src/utils/getConditionString.cjs
@@ -1,10 +1,10 @@
-const getConditionString = (payload) => {
+const getConditionString = (payload, separator = " AND ") => {
   const conStrArr = [];
   for (const key in payload) {
     const str = `${key} = '${payload[key]}'`;
     conStrArr.push(str);
   }
-  return conStrArr.join(" AND ");
+  return conStrArr.join(separator);
 };
 
 module.exports = getConditionString;
